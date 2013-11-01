aed399f6e220a305ac5835b3c0cea070c9ee6816
Chetan Jhaveri <cjhaveri@gmail.com>
2013-11-01 05:21:21 -0700

    meaningful integration test

--- src/test/java/test/chetan/integrationtest/SampleTestIT.java
+++ src/test/java/test/chetan/integrationtest/SampleTestIT.java
@@ -1,5 +1,15 @@
 package test.chetan.integrationtest;
+import java.util.ArrayList;
+
+import javax.ws.rs.core.Response;
+
+import org.apache.cxf.jaxrs.client.WebClient;
+import org.codehaus.jackson.jaxrs.JacksonJaxbJsonProvider;
+import org.springframework.http.MediaType;
+import org.testng.Assert;
 import org.testng.annotations.*;
+
+import test.chetan.model.Stock;
 
 /**
  * This is an integration test.
@@ -11,6 +21,17 @@ public class SampleTestIT {
 
 	@Test
 	public void helloWorldTest() {
-		System.out.println("happy run for integration test!");
+		
+		JacksonJaxbJsonProvider jsonProvider = new JacksonJaxbJsonProvider();
+		ArrayList<JacksonJaxbJsonProvider> providers = new ArrayList<JacksonJaxbJsonProvider>();
+		providers.add(jsonProvider);
+		
+		WebClient client = WebClient.create("http://localhost:8080",providers);
+		client.path("sample-web/service/stockservice/stock/{id}","1");
+		//client.accept("application/json");
+		Response r = client.get();
+		Stock stock = r.readEntity(Stock.class);
+		Assert.assertNotNull(stock);
+		
 	}
 }
