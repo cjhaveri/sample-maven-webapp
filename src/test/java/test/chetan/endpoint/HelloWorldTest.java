5feec3abcc64ab11259274e920c9b3d3a9a77685
Chetan Jhaveri <cjhaveri@gmail.com>
2014-10-06 17:06:32 -0700

    incomplete changes to get sonar to display integration and unit test coverage

--- src/test/java/test/chetan/endpoint/HelloWorldTest.java
+++ src/test/java/test/chetan/endpoint/HelloWorldTest.java
@@ -12,58 +12,79 @@ import test.chetan.dto.StockDTO;
 import test.chetan.endpoint.StockServiceEndpoint;
 import test.chetan.model.Stock;
 import test.chetan.repository.CommentsRepository;
+import test.chetan.repository.MiscRepository;
 import test.chetan.repository.StockRepository;
 
 /**
- * This is an integration test.
- * the maven-surefire-plugin will look for this, as it will pattern match Test.java
+ * This is an integration test. the maven-surefire-plugin will look for this, as
+ * it will pattern match Test.java
+ * 
  * @author chetanjhaveri
- *
+ * 
  */
 public class HelloWorldTest {
 
 	@Test
 	public void testStockWithEmptyFields() {
-	
+
 		StockDTO crappyStock = new StockDTO();
 		StockServiceEndpoint endPoint = new StockServiceEndpoint();
 		Response response = endPoint.addStockInformation(crappyStock);
-		
-		Assert.assertEquals(response.getStatus(), Response.noContent().build().getStatus());
-		
-		
+
+		Assert.assertEquals(response.getStatus(), Response.noContent().build()
+				.getStatus());
+
 	}
-	
+
 	@Test
-	public void testStockWithPopulatedFields(){
-	
+	public void testStockWithPopulatedFields() {
+
 		StockDTO goodStock = new StockDTO();
 		goodStock.setCompanyName("IBM Corporation");
 		goodStock.setTickerSymbol("IBM");
 		goodStock.setComments("Hey Hey");
-		
+
 		Stock mockStock = new Stock();
 		mockStock.setCompanyName("IBM Corporation");
 		mockStock.setTickerSymbol("IBM");
-	
+
 		StockRepository mockRepository = mock(StockRepository.class);
-		
+
 		CommentsRepository commentsRepository = mock(CommentsRepository.class);
-		
-		//look into why this is not needed anymore!
-		//when(mockRepository.save(any(Stock.class))).thenReturn(mockStock);
-	
+
+		TransactionalMisc misc = mock(TransactionalMisc.class);
+
+		// look into why this is not needed anymore!
+		// when(mockRepository.save(any(Stock.class))).thenReturn(mockStock);
+
 		StockServiceEndpoint endPoint = new StockServiceEndpoint();
-		//set the mock repository in
+		// set the mock repository in
 		endPoint.stockRepository = mockRepository;
 		endPoint.commentRepository = commentsRepository;
-		
-		
+		endPoint.misc = misc;
+
 		Response response = endPoint.addStockInformation(goodStock);
-		
+
 		verify(mockRepository, times(1)).save(any(Stock.class));
-		Assert.assertEquals(response.getStatus(), Response.ok().build().getStatus());
-		
-	
+		Assert.assertEquals(response.getStatus(), Response.ok().build()
+				.getStatus());
+
+	}
+
+	@Test
+	public void testDelete() {
+
+		StockRepository mockRepository = mock(StockRepository.class);
+
+		StockServiceEndpoint endPoint = new StockServiceEndpoint();
+		// set the mock repository in
+		endPoint.stockRepository = mockRepository;
+
+		Response response = endPoint.deleteAllStocks();
+
+		verify(mockRepository, times(1)).deleteAllData();
+		Assert.assertEquals(response.getStatus(), Response.ok().build()
+				.getStatus());
+
 	}
 }
